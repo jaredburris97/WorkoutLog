6572ad49c40555a1e761b158a1d7ad43b131c416
Jared Burris <jaredburris97@gmail.com>
2017-09-05 13:47:51 -0400

    Added middleware (CORS)

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(require('./middleware/headers'));
+
 app.use('/api/test', function(req,res){
 	res.send('Hello World')
-})
+});
 
 app.listen(3000, function(){
-	console.log("Listening on 3000...");
-})
+	console.log("...App is listening (open) on 3000...");
+});
+
