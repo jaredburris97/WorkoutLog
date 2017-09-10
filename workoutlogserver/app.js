77f8b8ffcd191279362a0f7f6a4dc1d4ca5f3b0c
Jared Burris <jaredburris97@gmail.com>
2017-09-10 15:26:55 -0400

    Completed Module 10

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -3,8 +3,10 @@ var app = express();
 var bodyParser = require('body-parser'); // added in module 7
 var sequelize = require('./db.js');		// added in module 9
 
-var User = sequelize.import('./models/user');
+var User = sequelize.import(__dirname + '\\models\\user');
+
 //Create table
+
 User.sync();	//User.sync({force:true});	//WARNING: THIS DROPS(DELETES) USER TABLE
 
 app.use(bodyParser.json());
@@ -13,8 +15,11 @@ app.use('/api/user',require('./routes/user'))
 app.use('/api/test', function(req,res){
 	res.send('Hello World, from server app.js!')
 });
+
 //sequelize code cut to db.js file (module 9)
+
 app.listen(3000, function(){
 	console.log("...App is listening (open) on 3000...");
 });
+
 //app.post code cut to user.js in routes folder (module 10)
