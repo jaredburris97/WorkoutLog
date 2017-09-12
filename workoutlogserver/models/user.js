e1db91ca55ab466450788a77817f30cc5d1c8583
Jared Burris <jaredburris97@gmail.com>
2017-09-12 14:21:18 -0400

    stuff

---
+++ workoutlogserver/models/user.js
@@ -0,0 +1,20 @@
+// user model created using sequelize
+// talks to the table user
+
+// module.exports = function(sequelize, DataTypes){
+// 	var User = sequelize.define('user', {
+// 		username: Sequelize.STRING,
+// 		passwordhash: Sequelize.STRING
+// 	});
+// 	return User;
+// };
+
+//user model created using sequelize
+//talks to the table user
+
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define('user', {
+		username: DataTypes.STRING,
+		passwordhash: DataTypes.STRING
+	});
+};
